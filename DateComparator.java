d4922b69f1e3cea760ca5dc61b0ddc5eaa2fdb7c
limjeongmin <jml8740@naver.com>
2017-04-04 14:26:18 +0900

    Delete DateComparator.java

--- DateComparator.java
+++
@@ -1,8 +0,0 @@
-package com.sd.sort.v2;
-
-public class DateComparator implements Comparator {
-    public DateComparator() {}
-    public int compare(Object o1, Object o2) {
-      return ((Date) o1).compareTo((Date) o2);
-    }
-}
