6a158b77da34982314d36765d4e68ce7f5f006c8
limjeongmin <jml8740@naver.com>
2017-04-17 20:10:40 +0900

    Add files via upload

---
+++ src/headfirst/factory/pizzas/PepperoniPizza.java
@@ -0,0 +1,11 @@
+package headfirst.factory.pizzas;
+
+public class PepperoniPizza extends Pizza{
+  public PepperoniPizza(){
+    name="PepperoniPizza";
+    dough="Regular Crust";
+    sauce="Marinara Pizza Suace";
+    toppings.add("Peproni");
+    toppings.add("Parmesan");
+  }
+}
---
+++ src/headfirst/factory/pizzas/PotatoPizza.java
@@ -0,0 +1,11 @@
+package headfirst.factory.pizzas;
+
+public class PotatoPizza extends Pizza{
+  public PotatoPizza(){
+    name="PotatoPizza";
+    dough="Regular Crust";
+    sauce="Marinara Pizza Suace";
+    toppings.add("sliced Potato");
+    toppings.add("bacon");
+  }
+}
